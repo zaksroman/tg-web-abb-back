c1c8511fc836b3ebeae7d827dac2691f31578592
user <zaksroman@gmail.com>
2024-08-05 15:17:26 +0500

    start version for ARC

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 const mongoose = require('mongoose');
-const multer  = require('multer');
-const fs = require("fs");
 const Schema = mongoose.Schema
 
 const PORT = 8000
@@ -12,23 +10,11 @@ const PORT = 8000
 /////variables////
 const forumChatId = -1002105194325
 const token = '6476733091:AAGjoUeCRXN8GIQT8jMwvZkxYaXfVsWUxUk';
-const webAppUrl =  /*'https://silly-bubblegum-7266f3.netlify.app'*/ 'https://tg-bot-test.ru/'
-const db = {
-    username: 'admin',
-    password: 'Rk1*48Kz',
-    host: 'calecija.beget.app',
-    database: 'admin'
-}/*'mongodb://localhost:27017/learnBD'*/
-const getDataApi = '/api/getProducts'
-const postDataApi = '/api/postProduct'
-const delDataApi = '/api/delProduct/:_id'
-const editDataApi = '/api/editProduct'
 /////////////////
 
 app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
-app.use('/uploads', express.static('uploads'));
 
 ////////////
 const bot = new TelegramBot(token, {polling: true});
@@ -36,24 +22,13 @@ const bot = new TelegramBot(token, {polling: true});
 
 mongoose
     .connect(
-        `mongodb://${db.username}:${db.password}@${db.host}/${db.database}`
+        'mongodb://localhost:27017/learnBD'
     )
     .then(() => {
     console.log('Подключение к MongoDB успешно!');
 }).catch((error) => {
     console.error('Ошибка подключения к MongoDB:', error);
 });
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads')
-    },
-    filename: function (req, file, cb) {
-        cb(null, `${Date.now()}` )
-    }
-})
-
-const upload = multer({ storage: storage })
 
 const userDataSchema = new Schema({
     fromId: {
@@ -74,83 +49,7 @@ const userDataSchema = new Schema({
     },
 })
 
-const productSchema = new Schema({
-    title: String,
-    description: String,
-    price: Number,
-    images: Array,
-    count: Number,
-    createdAt: { type: Date, default: Date.now }
-});
-
 const userDataModel = mongoose.model('userDataModel', userDataSchema, 'pair_userbot_messages')
-const productModel = mongoose.model('productModel',productSchema, 'products')
-
-///API///
-app.get(getDataApi, async (req, res) => {
-    try {
-        const products = await productModel.find().sort({ createdAt: -1 })
-        res.json(products)
-    } catch (e) {
-        res.status(500).json({ message: e.message });
-    }
-})
-
-app.post(postDataApi, upload.array('images', 5), async (req, res) => {
-    try {
-        const {title, description, price} = req.body;
-        const images = req.files.map(file => file.filename);
-
-        const newProduct = new productModel({
-            title: title,
-            price: price,
-            description: description,
-            images: images,
-        })
-
-        const savedProduct = await newProduct.save();
-
-        res.status(201).json(savedProduct)
-    } catch (e) {
-        res.status(400).json({ message: e.message })
-    }
-})
-
-app.delete(delDataApi,  async (req, res) => {
-    try {
-        const deletedProduct = await productModel.findByIdAndDelete(req.params._id);
-
-        if (!deletedProduct) {
-            return res.status(404).json({ message: "Товар не найден" });
-        }
-        deletedProduct.images.forEach(image => {
-            const imagePath = `uploads/${image}`;
-            if (fs.existsSync(imagePath)) {
-                fs.unlinkSync(imagePath);
-            }
-        })
-        res.status(200).json({ message: "Товар успешно удален", deletedProduct });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-app.put(`${editDataApi}/:_id`, upload.array('images', 5),async (req, res) => {
-    try {
-        const updatedProperties = req.body; // Получаем обновленные свойства товара из тела запроса
-
-        const updatedProduct = await productModel.findOneAndUpdate(
-            { _id: req.params._id },
-            { $set: updatedProperties },
-            { new: true }
-        );
-
-        res.status(200).json({ message: 'Товар успешно обновлен', updatedProduct });
-    } catch (error) {
-        console.error('Ошибка при обновлении товара:', error);
-        res.status(500).json({ message: 'Внутренняя ошибка сервера' });
-    }
-});
 
 //////////MESSAGE///
 bot.on('message', async (msg) => {
@@ -167,17 +66,7 @@ bot.on('message', async (msg) => {
 
     try {
         if (text === '/start') {
-            const sendWelcome = await bot.sendMessage(botChatId, 'Добро пожаловать в магазин)', {
-                reply_markup: {
-                    keyboard: [
-                        [{text: 'Магазин', web_app: {url: webAppUrl}}]
-                    ],
-                    resize_keyboard: true,
-                    one_time_keyboard: false
-                }
-            })
-
-            const sendInstruction = await bot.sendMessage(botChatId, 'Нажмите на кнопку справа в поле ввода, чтоб вызвать магазин или начать набор текста')
+            const sendWelcome = await bot.sendMessage(botChatId, 'Добро пожаловать в поддержку')
 
             const existingUserData = await userDataModel.findOne({ fromId: msg.from.id });
             if (!existingUserData) {
@@ -201,8 +90,7 @@ bot.on('message', async (msg) => {
                 await userDataModel.findOneAndUpdate(
                     {fromId: msg.from.id},
                     {$push: {messages: {$each: [
-                                    {botMsg: sendWelcome.message_id},
-                                    {botMsg: sendInstruction.message_id}
+                                    {botMsg: sendWelcome.message_id}
                                 ]}}}
                 )
             }
@@ -253,22 +141,6 @@ bot.on('message', async (msg) => {
             console.error(e)
         }
     }
-
-    /// buy data
-    if (msg?.web_app_data?.data) {
-        try {
-            const data = JSON.parse(msg?.web_app_data?.data)
-            await bot.sendMessage(botChatId, 'Список ваших покупочек и информация по ним TEST')
-            await bot.sendMessage(forumChatId, '\u2757 Вся информация о заказе и пожеланиях клиента', {
-                message_thread_id: userBD.message_thread_id
-            })
-
-            setTimeout(async ()=> {
-                await bot.sendMessage(botChatId,'Всю информацию вы получите в этом чате')
-            }, 500)
-        } catch (e) {
-        }
-    }
 });
 
 /////////EDITMESSAGE/////////
@@ -281,8 +153,6 @@ bot.on('edited_message', async (editedMsg) => {
                 console.error('Ошибка поиска userDataModel в EDITMESSAGE', error)})
             : await userDataModel.findOne({fromId: userId}).catch((error) => {
                 console.error('Ошибка поиска userDataModel в EDITMESSAGE', error)})
-
-        // console.log(editedMsg)
 
         const findPairToEdit = async (editedMsg) =>{
             return await userBD?.messages?.find(el => el.msg === editedMsg?.message_id).botMsg
